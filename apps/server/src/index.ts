11dd5932779b29e81fe73ab757f0eaf2029a1cf8
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-05-01 16:17:45 -0300

    feat(test) get one video

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("test")
+import * as edgedb from "edgedb";
+import { Video } from "../dbschema/interfaces"
+
+const client = edgedb.createClient();
+
+async function main() {
+  const result = await client.querySingle<Video>(`
+    select Video {
+      title
+    } limit 1
+  `);
+
+  console.log(JSON.stringify(result, null, 2));
+}
+
+main();
